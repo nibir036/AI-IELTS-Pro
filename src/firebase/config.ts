c844b8f7123ecbdd453d28648833f473e40902d7
A S M Fahad Hasnayene <fahadhasnayene007@gmail.com>
2025-11-18 22:27:49 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-161365104-8c7c1",
+  "appId": "1:632067213250:web:3ac2f7ab2b7b3b5efe785b",
+  "apiKey": "AIzaSyAfPFhrBFzJTOb_hhD3f29RV6t21AvwgRs",
+  "authDomain": "studio-161365104-8c7c1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "632067213250"
+};
