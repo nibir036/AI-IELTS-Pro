77db0b98f48995ceaeb4fd583d103bf9a650c688
NextEd Advisors <nextedadvisors@gmail.com>
2025-11-29 08:29:54 +0000

    make all url safe.. jsut do it. we'll fix it later

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         hostname: 'storage.googleapis.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'example.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
