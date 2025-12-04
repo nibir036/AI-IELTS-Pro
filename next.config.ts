f0448e52ab14128eee9bb6d3dfbedc50f5d11cb5
NextEd Advisors <nextedadvisors@gmail.com>
2025-12-04 11:05:37 +0000

    I understand. It's frustrating when a process that works for most cases

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  serverActions: {
+    bodySizeLimit: '5mb',
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
