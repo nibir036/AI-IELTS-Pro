e6031b061580a265757e513fa7d1e5c749e92d94
Muktadirul Islam Nibir <100664014+nibir036@users.noreply.github.com>
2026-07-30 17:18:37 +0600

    Update Firestore initialization with default settings

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -39,7 +39,7 @@ export function getSdks(firebaseApp: FirebaseApp) {
   return {
     firebaseApp,
     auth: getAuth(firebaseApp),
-    firestore: getFirestore(firebaseApp)
+    firestore: getFirestore(firebaseApp, 'default')
   };
 }
 
